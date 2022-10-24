a621ac45c9bbecf3a0e27fbadbf3524d63f416b2
Gabriel Ávila Rangel <gabriel@sevenapps.com.br>
2022-10-24 17:13:18 -0400

    feat: first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+ 
+  return (
+    <NavigationContainer>
+     
+     
+    </NavigationContainer>
+  );
+};
+
+
+
+export default App;
